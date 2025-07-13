78508ab4b1aa6e8c3405303a2dd5e9b7305cfa96
'Leeheewon' <'gmldnjs1616@hanmail.net'>
2025-07-13 15:37:49 +0900

    java_GUI 활용-메뉴추천

---
+++ TeamProject_Java/src/menu_recommend/GUIMain.java
@@ -0,0 +1,15 @@
+package menu_recommend;
+
+public class GUIMain {
+
+	public static void main(String[] args) {
+		
+		MenuRecommendGUI gui = new MenuRecommendGUI();
+		// 만들어진 gui 에 controller에 작성된 이벤트 기능들을 부여
+		MenuController controller = new MenuController(gui);
+		
+		// GUI 활성
+		gui.showGUI();
+		
+	} // main
+} // class
